454eee0d9b5bd33846f65668a90f0a413f8c4b90
brunof5 <101609538+brunof5@users.noreply.github.com>
2023-02-05 10:47:11 -0300

    Update gerar_solucao_aleatoria.h

--- Macroentrega 1/gerar_solucao_aleatoria.h
+++ Macroentrega 1/gerar_solucao_aleatoria.h
@@ -26,40 +26,56 @@ void gerar_solucao_aleatoria(int SIZE){
 	vector<int> *rotas = new vector<int>[qtd_veiculos];
 	vector<int> random_numbers;
 	
+	// cria-se um vector dos pedidos de coleta
+	for(int i = 0; i < (SIZE - 1) / 2; i++)
+		random_numbers.push_back(i + 1);
+		
+	// randomiza os numeros nas posicoes do vector
+	random_shuffle(random_numbers.begin(), random_numbers.end());
+	
+	int tamanho_rota = 0, num_pedidos = (SIZE - 1) / 2, pedidos_atendidos = 0;
+	
 	for(int i = 0; i < qtd_veiculos; i++){
 		
-		// cria-se um vector de 1 até o numero de vertices - 1 (desconsidera o deposito unico)
-		for(int j = 0; j < SIZE - 1; j++)
-			random_numbers.push_back(j + 1);
+		cout << "Digite o numero de pedidos da rota (numeros de pedidos disponiveis = " << ((SIZE - 1) / 2) - pedidos_atendidos << "): ";
+		cin >> tamanho_rota;
 		
-		// randomiza os numeros nas posicoes do vector
-		random_shuffle(random_numbers.begin(), random_numbers.end());
+		arq_solucao << (tamanho_rota * 2) + 2 << " ";
 		
+		int j = 0;
+		while(pedidos_atendidos < num_pedidos and j < tamanho_rota){
+			
+			rotas[i].push_back(random_numbers[pedidos_atendidos]);
+			pedidos_atendidos++;
+			j++;
+		}
 		// insere-se 0 no inicio e no fim da rota para representar o local de origem e destino (deposito unico)
-		rotas[i] = random_numbers;
 		rotas[i].insert(rotas[i].begin(), 0);
 		rotas[i].push_back(0);
 		
-		random_numbers.clear();
+		bool primeiro_zero = false;
 		
-		/* insere-se as informacoes no arquivo das rotas
-		 * SIZE + 1, pois tem-se inicialmente SIZE - 1 (desconsiderando o deposito unico)
-		 * ao realizar a insercao do 0 no inicio e no fim, temos + 2 vertices para percorrer
-		 * SIZE - 1 + 2 = SIZE + 1 elementos em cada rota
-		 */
-		for(int k = 0; k < SIZE + 1; k++){
+		// insere-se as informacoes no arquivo das rotas
+		for(int k = 0; k < tamanho_rota + 2; k++){
 			
-			if(k == SIZE)
-				arq_solucao << rotas[i][k];
-			
-			else
+			if(rotas[i][k] == 0 and !primeiro_zero){
+				
 				arq_solucao << rotas[i][k] << " ";
+				primeiro_zero = true;
+			}
+			
+			else if(rotas[i][k] == 0 and primeiro_zero)
+				arq_solucao << rotas[i][k];
+				
+			else{
+				
+				arq_solucao << rotas[i][k] << " " << rotas[i][k] + ((SIZE - 1) / 2) << " ";
+			}
 		}
 		
 		arq_solucao << '\n';
+		cout << endl;
 	}
-	
-	arq_solucao << "EOF";
 	
 	arq_solucao.close();
 	delete[] rotas;
